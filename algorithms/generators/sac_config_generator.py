d007945d62bf8081338277202d5bc4ecf7a8c870
Federica Filippini <federica.filippini@polimi.it>
2024-05-22 10:54:00 +0200

    raise warning if the useless model field is provided (& updated sample dictionary accordingly)

--- algorithms/generators/sac_config_generator.py
+++ algorithms/generators/sac_config_generator.py
@@ -1,5 +1,5 @@
 """
-Copyright 2024 Federica Filippini
+Copyright 2024 Mohanad Diab, Federica Filippini
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -16,15 +16,23 @@ limitations under the License.
 from algorithms.generators.dqn_config_generator import DQNConfigGenerator
 from utilities.logger import Logger
 
-from ray.rllib.algorithms.dqn.dqn import calculate_rr_weights
-from ray.rllib.algorithms import AlgorithmConfig
-
 
 class SACConfigGenerator(DQNConfigGenerator):
   def __init__(
       self, logger: Logger = Logger(name="RL4CC-AlgoConfigGenerator")
     ):
-     
-    self.algo = "SAC"
     super().__init__(logger)
+    self.algo = "SAC"
+  
+  def validate_key_usage(self, all_params: dict):
+    using_suggested_keys, using_protected_keys = super().validate_key_usage(
+      all_params
+    )
+    if "model" in all_params:
+      self.logger.warn(
+        "SAC has two fields to configure for custom models: "
+        "`policy_model_config` and `q_model_config`, the `model` field of the "
+        "config is ignored"
+      )
+    return using_suggested_keys, using_protected_keys
     
