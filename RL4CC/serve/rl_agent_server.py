cea88d48633bfedfb36124fb0f433e44b95b8671
FFede0 <federica.filippini@unimib.it>
2026-03-27 11:43:17 +0100

    license info

--- RL4CC/serve/rl_agent_server.py
+++ RL4CC/serve/rl_agent_server.py
@@ -1,3 +1,18 @@
+"""
+Copyright 2026 Federica Filippini
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 from RL4CC.utilities.common import load_config_file, NumpyEncoder
 from RL4CC.log_and_report.rl4cc_logger import Logger
 from RL4CC.algorithms.algorithm import Algorithm
