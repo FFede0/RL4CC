d45814ce188c7de8708fb80fff70a386f9872a1d
FFede0 <federica.filippini@unimib.it>
2026-09-16 13:47:01 +0200

    avoid errors with missing imports

--- RL4CC/algorithms/backends/__init__.py
+++ RL4CC/algorithms/backends/__init__.py
@@ -1,4 +1,13 @@
-from RL4CC.algorithms.backends.morl_algorithm_backend import (
-  MORLAlgorithmBackend
-)
-from RL4CC.algorithms.backends.ray_algorithm_backend import RayAlgorithmBackend
+# Ray RLLib-based
+try:
+  from RL4CC.algorithms.backends.ray_algorithm_backend import RayAlgorithmBackend
+except ImportError:
+  pass
+
+# MORL-Baselines-based
+try:
+  from RL4CC.algorithms.backends.morl_algorithm_backend import (
+    MORLAlgorithmBackend
+  )
+except ImportError:
+  pass
